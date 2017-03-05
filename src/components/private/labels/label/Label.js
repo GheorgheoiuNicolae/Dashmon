5a735e73cec283335c7f8efa7932fb1a85f78f53
Gabriela Bozbici <gabrielabozbici@Gheorgheoius-MacBook-Pro.local>
2017-03-05 14:13:42 +0100

    added color picker for labels

--- src/components/private/labels/label/Label.js
+++ src/components/private/labels/label/Label.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
+import LabelColor from '../LabelColor/LabelColor';
 import * as action from '../../../../actions/label';
 
 export default class Label extends Component {
+  componentWillMount(){
+    this.setState({
+      isOnHover: false,
+      color: ''
+    })
+  }
+
   removeLabel(){
     this.props.dispatch(action.removeLabel(this.props.uid, this.props.data.id));
   }
 
+  enableHover(){
+    this.setState({
+      isOnHover: true
+    })
+  }
+
+  disableHover(){
+    this.setState({
+      isOnHover: false
+    })
+  };
+  setColor(color){
+    this.setState({color: color})
+  }
+
   render () {
+    let classes = this.state.isOnHover ? 'label-colors show' : 'label-colors hide';
+
     return (
-      <div className='Label'>
-        <span className="label-title">{this.props.data.title}</span>
-        <span className="remove" onClick={this.removeLabel.bind(this)}>X</span>
+      <div className='Label' 
+        onMouseEnter={this.enableHover.bind(this)}
+        onMouseLeave={this.disableHover.bind(this)}
+        >
+        <div className="colorPicked" style={{backgroundColor: `${this.state.color.code}`}}></div>
+        <div className="label-title">{this.props.data.title}</div>
+        <div className={classes}>
+          <LabelColor getColor={this.setColor.bind(this)}  />
+        </div>
+        <div className="remove" onClick={this.removeLabel.bind(this)}>X</div>
       </div>
     )
   }
